e4a471c26f7999e5558808d4c6110e9f6ca2b83b
sasaneprataik648@gmail.com <sasanepratik648@gmail.com>
2025-01-06 11:44:08 +0530

    new commit

--- Data/database.js
+++ Data/database.js
@@ -5,7 +5,7 @@ export const connectdb = ()=>{
   .connect(process.env.MONGO_URI, {
     dbName: "backendapi",
   })
-  .then(() => console.log("Database connected"))
+  .then((c) => console.log(`Database connected with ${c.connection.host}`))
   .catch((e) => console.log(e));
 }
 
